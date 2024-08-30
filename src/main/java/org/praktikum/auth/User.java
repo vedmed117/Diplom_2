92e9fd25faed14e260fb90dced39ba16edd3e354
Maxim Medvedev <vedmed117@yandex.kz>
2024-08-31 00:54:07 +0500

    Added pom.xml Added User.java

---
+++ src/main/java/org/praktikum/auth/User.java
@@ -0,0 +1,38 @@
+package org.praktikum.auth;
+
+public class User {
+    private String email;
+    private String password;
+    private String name;
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    // Геттеры и сеттеры
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
